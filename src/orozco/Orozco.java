68ffe4f26923a198fe7b309ad1775aecea542a1c
rugsmunny <karim.sendesni@gmail.com>
2020-02-03 16:08:56 +0100

    test 3

--- src/orozco/Orozco.java
+++ src/orozco/Orozco.java
@@ -15,7 +15,7 @@ public class Orozco {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        System.out.println("HEllo Burld");  
+        System.out.println("blääää");}
     
 }
