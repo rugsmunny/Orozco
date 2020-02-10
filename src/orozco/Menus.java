61c4e7eed0eb2cf603664742dd011237e61c2a97
ECULeo <lwyh1999@gmail.com>
2020-02-10 10:29:24 +0100

    changes in statistic

--- src/orozco/Menus.java
+++ src/orozco/Menus.java
@@ -25,11 +25,8 @@ public class Menus {
         return question;
     }
 
-
     //public static String setName() {
-
     //}
-
     public static int choiceMethod(String[] choices, Scanner input, boolean back) {
         int choice;
         while (true) {
@@ -41,7 +38,6 @@ public class Menus {
             if (back == true) {
                 question += "\n0. Back";
             }
-
 
             question += "\n\nChoice: ";
 
@@ -116,7 +112,7 @@ public class Menus {
         while (true) {
             clearScreen();
             name = questionAndInputString("Name", input);
-             if (name.length() < 2) {
+            if (name.length() < 2) {
                 continue;
             }
             try {
@@ -142,7 +138,6 @@ public class Menus {
         clearScreen();
         int salary = questionAndInputInt("Salary", input);
         Employee.genders gender = null;
-
 
         clearScreen();
 
@@ -245,7 +240,7 @@ public class Menus {
     }
 
     public static void selectEmployee(Scanner input) {
-clearScreen();
+        clearScreen();
         String employee;
         int index = 1;
         for (Employee e : Orozco.employees) {
@@ -278,57 +273,93 @@ clearScreen();
     }
 
     public static void statistics(Scanner input) {
-clearScreen();
-        switch (Menus.choiceMethod(new String[]{"Gender", "Wages"}, input, true)) {
+        clearScreen();
+        boolean looping = true;
+        while (looping == true) {
+            switch (Menus.choiceMethod(new String[]{"Gender", "Wages"}, input, true)) {
 
-            case 1:
-clearScreen();
-                getGenderCount(Menus.choiceMethod(new String[]{"Men", "Women", "Non Binary"}, input, true));
-                break;
-            case 2:
-clearScreen();
-                getSalary(Menus.choiceMethod(new String[]{"Highest salary", "Lowest salary", "Average salary"}, input, true));
-                break;
-            case 3:
-                break;
+                case 1:
+                    clearScreen();
+                    //getGenderCount(Menus.choiceMethod(new String[]{"Men", "Women", "Non Binary"}, input, true));
+                    getGenderCount();
+
+                    break;
+                case 2:
+                    clearScreen();
+                    getSalary(Menus.choiceMethod(new String[]{"Highest salary", "Lowest salary", "Average salary"}, input, true));
+                    break;
+                case 0:
+                    looping = false;
+            }
         }
+
     }
 
 //    public static int getGenderStats(){
 //        Orozco.employees.stream().collect(Collector.)
 //    }
-    public static void getGenderCount(int genderChoice) {
+    public static void getGenderCount() {
 
         Employee.genders gender = null;
         String genDer = null;
-        switch (genderChoice) {
-            case 1:
-                gender = Employee.genders.man;
-                genDer = "men";
-                break;
-            case 2:
-                gender = Employee.genders.woman;
-                genDer = "women";
-                break;
-            case 3:
-                gender = Employee.genders.nonbinary;
-                genDer = "nonbinaries";
-                break;
-        }
 
-        int num = 0;
+        int men = 0;
+        int women = 0;
+        int nonbinary = 0;
+
         for (Employee e : Orozco.employees) {
-
-            if (e.gender.equals(gender)) {
-
-                num += 1;
-
+            if (e.gender.equals(Employee.genders.man)) {
+                men++;
             }
-
+            if (e.gender.equals(Employee.genders.woman)) {
+                women++;
+            }
+            if (e.gender.equals(Employee.genders.nonbinary)) {
+                nonbinary++;
+            }
         }
-        clearScreen();
-        System.out.println("Total number of " + genDer + " is " + num);
 
+        int total = women + men + nonbinary;
+
+        float menpercent = ((float) men / (float) total) * 100;
+        float womenpercent = ((float) women / (float) total) * 100;
+        float nonbinarypercent = ((float) nonbinary / (float) total) * 100;
+
+        System.out.println(String.format("Number of men is: %d(%.1f%%)\nNumber of women is: %d(%.1f%%)\nNumber of nonbinary is: %d(%.1f%%)", men, menpercent, women, womenpercent, nonbinary, nonbinarypercent));
+
+        /*
+         switch (genderChoice) {
+         case 1:
+         gender = Employee.genders.man;
+         genDer = "men";
+         break;
+         case 2:
+         gender = Employee.genders.woman;
+         genDer = "women";
+         break;
+         case 3:
+         gender = Employee.genders.nonbinary;
+         genDer = "nonbinaries";
+         break;
+         }
+               
+        
+        
+         System.out.println();
+
+         int num = 0;
+         for (Employee e : Orozco.employees) {
+
+         if (e.gender.equals(gender)) {
+
+         num += 1;
+
+         }
+
+         }
+         clearScreen();
+         System.out.println("Total number of " + genDer + " is " + num);
+         */
     }
 
     public static void getSalary(int salaryChoice) {
